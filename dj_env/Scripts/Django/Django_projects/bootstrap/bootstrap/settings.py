90cf63c4c3a5b1447ce3ecf4cdac924d8cb6b3d1
MJShreelakshmi <mjshreelakshmii@gmail.com>
2024-12-18 19:17:38 +0530

    updates

--- dj_env/Scripts/Django/Django_projects/bootstrap/bootstrap/settings.py
+++ dj_env/Scripts/Django/Django_projects/bootstrap/bootstrap/settings.py
@@ -119,6 +119,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_DIR_BOOTSTRAP = os.path.join(os.path.join(BASE_DIR, 'bootstrap'), 'static')
+STATIC_DIR_APP = os.path.join(os.path.join(BASE_DIR, 'app'), 'static')
+STATICFILES_DIRS = [STATIC_DIR_BOOTSTRAP, STATIC_DIR_APP]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
